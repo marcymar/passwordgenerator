21e5d60f6fe17647b4562116f780ca22decfad07
Marcella Ash <marcella@ash-photos.com>
2020-07-23 15:58:09 -0700

    new PW array, rndomize characters, script comm out

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+// var generateBtn = document.querySelector("#generate");
 
-}
+// // Write password to the #password input
+// function writePassword() {
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// h e l p 
+
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+
+//     passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
