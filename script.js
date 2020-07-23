7e86643036eff9d4707d2e187f918a3895c25c39
Marcella Ash <marcella@ash-photos.com>
2020-07-23 16:26:45 -0700

    movedfrom old file to this one

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 // Assignment Code
+function generate() {
+    // var generateBtn = document.querySelector("#generate");
 
 
-// var generateBtn = document.querySelector("#generate");
+    // set password length
+    let complexity = document.getElementById("slider").value;
 
-// // Write password to the #password input
-// function writePassword() {
 
-// h e l p 
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$?&*()[]{}^~";
 
-//     var password = generatePassword();
-//     var passwordText = document.querySelector("#password");
+    let password = "";
 
-//     passwordText.value = password;
+    // for loop to choose pw characters
+    for (var i = 1; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
 
-// }
+    // add pw to textbox
+    document.getElementById("generate").value = password;
+    document.getElementById("password").innerHTML += password;
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+    // add password to previous pw section
+    document.getElementById("lastpw").innerHTML += password + "<br>";
+}
+
+// default length display of slider 60
+document.getElementById("length").innerHTML = "Length: 60";
+
+// function to set slider
+document.getElementById("slider").oninput = function () {
+
+    if (document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else {
+        document.getElementById("length").innerHTML = "Length: 8";
+    }
